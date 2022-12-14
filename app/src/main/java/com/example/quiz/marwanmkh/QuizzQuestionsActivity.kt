f15b4b4649b974345047a1f839560f52a27fef39
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2022-12-15 00:55:06 +0300

    set options in red

--- app/src/main/java/com/example/quiz/marwanmkh/QuizzQuestionsActivity.kt
+++ app/src/main/java/com/example/quiz/marwanmkh/QuizzQuestionsActivity.kt
@@ -1,13 +1,20 @@
 package com.example.quiz.marwanmkh
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 
-class QuizzQuestionsActivity : AppCompatActivity() {
+class QuizzQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var myCurrentPosition : Int = 1;
+    private var questionsList:ArrayList<Question>? = null;
+    private var mySelectedOptionSelected : Int? = null;
 
     private var progressBar : ProgressBar? = null;
     private var tvProgess : TextView? = null;
@@ -17,12 +24,13 @@ class QuizzQuestionsActivity : AppCompatActivity() {
     private var optionThree : TextView? = null;
     private var optionFour : TextView? = null;
     private var imageView : ImageView? = null;
+    private var submitButton : Button? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizz_questions)
 
-        val questionsList = Constants.getQuestion();
+        questionsList = Constants.getQuestion();
         progressBar = findViewById(R.id.progress_bar);
         tvProgess = findViewById(R.id.tv_progress);
         questionString = findViewById(R.id.tv_question);
@@ -33,13 +41,20 @@ class QuizzQuestionsActivity : AppCompatActivity() {
         optionThree = findViewById(R.id.optionThree);
         optionFour = findViewById(R.id.optionFour);
 
-        var currentPosition = 1;
-        val question : Question = questionsList[currentPosition - 1];
+        submitButton = findViewById(R.id.btn_submit);
+
+        setQuestion();
+        defaultOptionsView();
+        //Log.i("numbers of text", "${questionsList.size}");
+    }
+
+    private fun setQuestion() {
+        val question: Question = questionsList!![myCurrentPosition - 1];
 
         imageView?.setImageResource(question.image);
 
-        progressBar?.progress = currentPosition;
-        tvProgess?.text = "$currentPosition / ${progressBar?.max}"
+        progressBar?.progress = myCurrentPosition;
+        tvProgess?.text = "$myCurrentPosition / ${progressBar?.max}"
 
         questionString?.text = question.question;
         optionOne?.text = question.optionOne;
@@ -47,8 +62,39 @@ class QuizzQuestionsActivity : AppCompatActivity() {
         optionThree?.text = question.optionThree;
         optionFour?.text = question.optionFour;
 
+        if(myCurrentPosition == questionsList!!.size){
+            submitButton?.text = "FINISH";
+        } else {
+            submitButton?.text = "SUBMIT";
+        }
+    }
 
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>();
 
-        //Log.i("numbers of text", "${questionsList.size}");
+        optionOne?.let{
+            options.add(0, it);
+        }
+
+        optionTwo?.let{
+            options.add(1, it);
+        }
+
+        optionThree?.let{
+            options.add(2, it);
+        }
+
+        optionFour?.let{
+            options.add(3, it);
+        }
+
+        for(option in options){
+            option.setTextColor(Color.RED);
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
