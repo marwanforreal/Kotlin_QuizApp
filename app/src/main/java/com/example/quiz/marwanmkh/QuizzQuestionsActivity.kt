f1e469d561b08e4c7ec4258cad95e13669162e8c
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2022-12-15 15:15:09 +0300

    set on click responsive design for options

--- app/src/main/java/com/example/quiz/marwanmkh/QuizzQuestionsActivity.kt
+++ app/src/main/java/com/example/quiz/marwanmkh/QuizzQuestionsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.quiz.marwanmkh
 
 import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class QuizzQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -40,6 +42,12 @@ class QuizzQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         optionTwo = findViewById(R.id.optionTwo);
         optionThree = findViewById(R.id.optionThree);
         optionFour = findViewById(R.id.optionFour);
+
+        optionOne?.setOnClickListener(this);
+        optionTwo?.setOnClickListener(this);
+        optionThree?.setOnClickListener(this);
+        optionFour?.setOnClickListener(this);
+        submitButton?.setOnClickListener(this);
 
         submitButton = findViewById(R.id.btn_submit);
 
@@ -90,11 +98,36 @@ class QuizzQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
         for(option in options){
             option.setTextColor(Color.RED);
+            option.typeface = Typeface.DEFAULT;
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            );
         }
 
     }
 
+    private fun selectedOptionView(tv:TextView, selectedOptionNum : Int){
+        defaultOptionsView();
+
+        mySelectedOptionSelected = selectedOptionNum;
+
+        tv.setTextColor(Color.MAGENTA);
+        tv.setTypeface(tv.typeface, Typeface.BOLD);
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+        when(v?.id){
+            R.id.optionOne -> { optionOne?.let { selectedOptionView(it, 1); } }
+            R.id.optionTwo -> { optionTwo?.let { selectedOptionView(it, 2); } }
+            R.id.optionThree -> { optionThree?.let { selectedOptionView(it, 3); } }
+            R.id.optionFour -> { optionFour?.let { selectedOptionView(it, 4); } }
+            R.id.btn_submit -> {//TODO
+            }
+        }
     }
 }
